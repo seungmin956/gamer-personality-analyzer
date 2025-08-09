035679e9584b6137e86eb8371c0c496749d11521
Chloe (Seungmin Lee) <seungminlee956@gmail.com>
2025-08-09 11:36:04 +0900

    Update model_server.py

--- ml-models/model_server.py
+++ ml-models/model_server.py
@@ -1,1 +1,10 @@
 
+from transformers import pipeline, AutoTokenizer, AutoModel
+import json
+
+# 여러 모델 성능 비교
+models_to_test = [
+    "klue/roberta-base",
+    "beomi/KcELECTRA-base", 
+    "sentence-transformers/xlm-r-base"
+]
